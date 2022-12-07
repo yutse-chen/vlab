a26d196c5cc9b935881d9c8885abe525b921f8d4
Noah Chen <noah15986@gmail.com>
2022-12-07 23:51:27 +0800

    'Update'

--- app.py
+++ app.py
@@ -1,50 +1,66 @@
-from flask import Flask, request, jsonify, redirect, url_for
+from flask import Flask, request, jsonify, redirect, url_for, session
 from flask import render_template
+
+# from flask_socketio import SocketIO, emit
+# from flask.ext.login import current_user, logout_user
+
 import os
 import numpy as np
-from numpy import genfromtxt
+from numpy import genfromtxt, fromstring
 import json
 import pprint
+import uuid
+
+
+
 pp = pprint.PrettyPrinter(width=41, compact=True)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
 app.config['UPLOAD_FOLDER'] = "C:\\Users\\Sean\\PycharmProjects\\vlab\\data"
+
 # private function
 def stp_parser():
-    raw_data = genfromtxt('./data/stp1.csv', delimiter=',', skip_header = 1, dtype=None)
-    data = raw_data[11:,]
-    cmd_count = (int(np.unique(raw_data[11:,2])[-1])+1)
-    find_delay = np.where((data[:,2] == "0"))[0]
-    for idx in range(len(find_delay)):
-        if find_delay[idx] + 1 != find_delay[idx+1]:
-            delay = find_delay[idx]+1
-            break
-    # switch
-    # led 24 - 34
-    # seg_0 35 - 43
-    # seg_1 44 - 52
-    # seg_2 53 - 61
-    # seg_3 62 - 70
-    cmd_sub_idx = [[24,34], [35, 43], [44, 52], [53, 61], [62, 70]]
-    output_key = ["led", "seg_0", "seg_1", "seg_2", "seg_3"]
-    output_seg = {"output":[]}
-    for cmd in range(cmd_count):
-        tmp = {}
-        if cmd == 0:
-            cmd_idx = cmd + 1
-        else:
-            cmd_idx = cmd * delay
-        for sub_idx, key in zip(cmd_sub_idx, output_key):
-            tmp[key] = [ int(l) for l in list((data[cmd_idx,sub_idx[0]:sub_idx[1]].astype(int) +1) % 2) ][::-1]
-            if key != "led":
-                tmp[key].insert(-2, tmp[key][-2])
-                tmp[key].insert(-2, tmp[key][-2])
-        output_seg["output"].append(tmp)
-    return output_seg
+    fp = './data/' + session.get('username') + ".csv"
+    if os.path.isfile(fp):
+        raw_data = genfromtxt(fp, delimiter=',', skip_header = 1, dtype=None)
+        data = raw_data[11:,]
+        cmd_count = (int(np.unique(raw_data[11:,2])[-1])+1)
+        find_delay = np.where((data[:,2] == "0"))[0]
+        for idx in range(len(find_delay)):
+            if find_delay[idx] + 1 != find_delay[idx+1]:
+                delay = find_delay[idx]+1
+                break
+        # switch
+        # led 24 - 34
+        # seg_0 35 - 43
+        # seg_1 44 - 52
+        # seg_2 53 - 61
+        # seg_3 62 - 70
+        cmd_sub_idx = [[24,34], [35, 43], [44, 52], [53, 61], [62, 70]]
+        output_key = ["led", "seg_0", "seg_1", "seg_2", "seg_3"]
+        output_seg = {"output":[]}
+        for cmd in range(cmd_count):
+            tmp = {}
+            if cmd == 0:
+                cmd_idx = cmd + 1
+            else:
+                cmd_idx = cmd * delay
+            for sub_idx, key in zip(cmd_sub_idx, output_key):
+                tmp[key] = [ int(l) for l in list((data[cmd_idx,sub_idx[0]:sub_idx[1]].astype(int) +1) % 2) ][::-1]
+                if key != "led":
+                    tmp[key].insert(-2, tmp[key][-2])
+                    tmp[key].insert(-2, tmp[key][-2])
+            output_seg["output"].append(tmp)
+        os.remove(fp)
+        session['rslt'] = output_seg
+        return output_seg
 
-
+@app.route('/get_rslt')
 def web_init():
-    pass
+    print(session.get('rslt'))
+    return json.dumps(session.get('rslt'), indent = 4)
 
 # @app.route('/')
 # def index():
@@ -54,9 +70,10 @@ def web_init():
 def entry():
     # GET request
     if request.method == 'GET':
-        args = request.args
+        # args = request.args
+        print("Try to load " + session.get('username') + ".csv")
         msg = stp_parser()
-        pp.pprint(msg)
+        # pp.pprint(msg)
         return json.dumps(msg, indent = 4)
     # POST request
     if request.method == 'POST':
@@ -65,21 +82,43 @@ def entry():
 
 from flask import send_from_directory
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('lab.html')
+    # session['messages'] = "False"
+    rslt = "False"
+    if session.get('messages') == "True":
+        print("Yee")
+        msg = session['messages']
+        rslt = session['rslt']
+    # print(f"msg={msg}")
+    session.clear()
+    # print("session: " + session.get('username'))
+    return render_template('lab.html', rslt=rslt)
+
+# @app.route('/init')
+# def init():
+#     resp = {'tmp_fn':str(uuid.uuid4())}
+#     return json.dumps(resp, indent = 4)
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    session['username'] = str(uuid.uuid4())
+    session['messages'] = "True"
     file = request.files['upload_file']
-    filename = "stp1.csv"
+    # filename = "stp1.csv"
+    fn = session.get('username') + ".csv"
+    # aa = fromstring(, dtype=int, sep=',')
+    # print(file.read().decode("utf-8"))
+    # print(fn)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], 
-                            filename))
+                            fn))
+    print(fn+ " has been saved.")
+    stp_parser()
     return redirect(url_for('index'))
 
 
--- static/lab.js
+++ static/lab.js
@@ -2,6 +2,25 @@ var stop = 0;
 var start_play = 1;
 var timeunit = 1000;
 var play_task;
+// var tmp_fn;
+
+function init()
+{
+  // console.log(tmp_fn);
+}
+
+document.getElementById('smit').onclick = function () {
+  var checkedPosition =
+      document.querySelectorAll("#player_stuff [name='position']:checked");
+  var playerId =
+      document.querySelector("#player_stuff [name='player_id']");
+  var position = checkedPosition[0].value;
+  var player_id = playerId.value;
+
+  document.getElementById('disp_player').innerHTML =
+      position + " " + player_id;
+}
+
 
 
 function checkUpload(){
@@ -53,7 +72,7 @@ function playProcLed(l) {
     // console.log(typeof(i));
 
     // if ( (l[i] != 1) || (parseInt(i,10) !== parseInt(9,10)) || (parseInt(i, 10) !== parseInt(8, 10)) ){
-    if ( l[i] != 1) {
+    if ( l[i] == 1) {
       $('.' + i + '.button-div').addClass('bg-red');
     }
     else
@@ -63,6 +82,13 @@ function playProcLed(l) {
   }
 }
 
+function clear_row(seg_length)
+{
+  for(i = 0;i < seg_length; i++)
+  {
+    $('#row'+i).removeClass('row-now');
+  }
+}
 
 async function playProc(fetchData) {
   var i = 0;
@@ -73,6 +99,8 @@ async function playProc(fetchData) {
     {
       do{
         // console.log(seg[i].seg_3);
+        clear_row(seg.length);
+        $('#row'+i).addClass('row-now');
         playProcSeg(3, seg[i].seg_3)
         playProcSeg(2, seg[i].seg_2);
         playProcSeg(1, seg[i].seg_1);
@@ -85,15 +113,18 @@ async function playProc(fetchData) {
 }
 
 function play() {
-
   if (start_play){
-    fetch('/test?arg=123')
-    .then(response => response.json())
-    .then(fetchData => {
-      // console.log(fetchData.seg)
-      playProc(fetchData)
-    })
-    .catch(console.error);
+    // console.log(tmp_fn);
+    // fetch('/test')
+    // fetch('/get_rslt')
+    // .then(response => response.json())
+    // .then(fetchData => {
+    //   // console.log(fetchData.seg)
+    //   playProc(fetchData)
+    // })
+    // .catch(console.error);
+    console.log(window.glob);
+    playProc(JSON.parse((window.glob).split('&#39;').join('"')));
   }
   else{
     timeunit = document.getElementById("timeunit").value;
@@ -105,7 +136,29 @@ function pause() {
   stop =1;
 }
 
+function parse_tbl(fetchData)
+{
+  var seg = fetchData.output;
+  for(i = 0;i < seg.length; i++)
+  {
+    console.log(seg[i].led);
+    var switch_txt = "";
+    for(j  = 0; j < (seg[i].led).length; j++)
+    {
+      // console.log((seg[i].led)[j]);
+      if ((seg[i].led)[j] == 1)
+      {
+        switch_txt = switch_txt + " " + j;
+      }
+    }
+    // console.log(switch_txt);
+    $('#tbody').append(`<tr id=row`+ i +`><th scope="row">`+ i +`
+    <td></td>
+    <td>` + switch_txt + `</td>
+    </th></tr>`);
+  }
 
+}
 
 function cleanAll() {
   console.log("[DEBUG] cleanAll")
@@ -121,56 +174,22 @@ function delTask() {
 
 function arrowUp() {
   console.log("[DEBUG] arrowUp")
+  // $('#tbody').append(`<tr><th scope="row">
+  // <td>0 2</td>
+  //  <td>1 2 5 6</td>
+  //  </th></tr>`);
+  fetch('/get_rslt')
+  .then(response => response.json())
+  .then(fetchData => {
+    parse_tbl(fetchData);
+    // console.log(fetchData)
+    // playProc(fetchData)
+  })
+  .catch(console.error);
+    
 }
 
 function arrowDown() {
   // play_task.abort();
   console.log("[DEBUG] arrowDown")
 }
-
-
-// $(document).ready(function ()
-//   {
-//       $("#myForm").submit(function (e)
-//           {
-//               //Stops submit button from refreshing page.
-//               e.preventDefault();
-
-//               var form_data = new FormData(this);
-
-//               $.ajax({
-//                   url: 'http://example/DB_1/AccessWeb/file_upload.php', //location of where you want to send image
-//                   dataType: 'json', // what to expect back from the PHP script, if anything
-//                   cache: false,
-//                   contentType: false,
-//                   processData: false,
-//                   data: form_data,
-//                   type: 'post',
-//                   success: function (response)
-//                       {
-//                           alert('success');
-//                       },
-//                   error: function ()
-//                       {
-//                           alert('failure');
-//                       }
-//               });
-//           });
-//   });
-
-
-// $(document).on('submit','#myForm',function(e)
-// {
-//   e.preventDefault();
-//   $.ajax({
-//     type:'POST',
-//     url:'/',
-//     data:{
-//       myfile:$("#myfile").val()
-//     },
-//       success:function()
-//     {
-//       alert('saved');
-//     }
-//   })
-// });
